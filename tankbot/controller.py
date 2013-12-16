e09eea848dc89444ae7cac3611c00b1754866cea
wcdolphin <wcdolphin@gmail.com>
2013-12-16 15:16:44 -0500

    Late speed and control updates

--- tankbot/controller.py
+++ tankbot/controller.py
@@ -11,16 +11,14 @@ from USBTank import USBTank
 class TankController():
 	def __init__(self, tank):
 		self.tank = tank
-		self.bounds = ((-1,1),(-1,1),(-1.5,.1))
+		self.bounds = ((-1,1),(-1,1),(-2,.1))
 		self.data = np.array([])
 		self.goal_dist = -1
 
 	def get_data(self):
 		(depth,_) = get_depth()
 		(raw_data, _) = depth2xyzuv(depth)
-		print len(raw_data)
 		self.data = np.array([point for point in raw_data[::4] if inBBox(point, self.bounds)])
-		print len(self.data)
 
 	def get_err(self):
 		if len(self.data):
@@ -36,14 +34,32 @@ class TankController():
 			while(True):
 				self.get_data()
 				rotErr, transErr = self.get_err()
-				print rotErr, transErr
 				if len(self.data) is 0:
 					tank.set_vel(0,0)
 					continue
-				if ((abs(rotErr) > .1) or (abs(transErr) > .1)) and transErr:
-					tank.set_vel(-20000*transErr, -30000*rotErr)
+				if transErr: # if there are points detected
+					linVel, angVel = self.get_speed(transErr, rotErr)
+					tank.set_vel(linVel, angVel)
 		except(KeyboardInterrupt):
 			self.tank.set_motors(0,0)
+
+	def get_speed(self, transErr, rotErr):
+		# proportional control
+		linVel = -100000*transErr
+		angVel = -100000*rotErr
+
+		# saturate
+		linVel = max(min(linVel, 32767), -32767)
+		angVel = max(min(angVel, 32767), -32767)
+
+		# dead zone
+		if abs(transErr) < .1:
+			linVel = 0
+		if abs(rotErr) < .1:
+			angVel = 0
+
+		return linVel, angVel
+
 
 
 def inBounds(val, bounds):
