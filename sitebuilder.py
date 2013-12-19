7e5a2346b396093ad40d2398a89324055d6d34d6
wcdolphin <wcdolphin@gmail.com>
2013-12-19 12:51:18 -0500

    Update sitebuilder so we can template

--- sitebuilder.py
+++ sitebuilder.py
@@ -2,11 +2,12 @@ import sys
 
 from flask import Flask, render_template
 from flask_frozen import Freezer
-
+import os
+import shutil
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['FREEZER_DESTINATION'] = "./"
+app.config['FREEZER_DESTINATION'] = "_build"
 freezer = Freezer(app)
 
 
@@ -14,8 +15,22 @@ freezer = Freezer(app)
 def index():
     return render_template('index.html')
 
+@app.route('/mechanical.html')
+def mechanical():
+    return render_template('mechanical.html')
+
+
+
+def copy_files(src, dest, ignore=None):
+    for f in os.listdir(src):
+        if not os.path.isdir(f):
+            shutil.copy(os.path.join(src,f),dest)
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
+        copy_files("./_build", "./")
+
     else:
         app.run(port=5000)
+
